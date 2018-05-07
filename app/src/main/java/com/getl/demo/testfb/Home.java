1d4487da175c8f5392f1f8f1eeda226a04827d13
Ashish <ashishkhobragade45@gmail.com>
2018-05-07 18:50:30 +0530

    copy splash screen from another project

--- app/src/main/java/com/getl/demo/testfb/Home.java
+++ app/src/main/java/com/getl/demo/testfb/Home.java
@@ -14,14 +14,7 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
-        List<String> list = new ArrayList<String>();
-        list.add("Romania");
-        list.add("Mexico");
-        list.add("Germany");
-        list.add("United Kingdom");
-        ArrayAdapter<String> dataAdapter= new ArrayAdapter<String>(this, R.layout.spinner_item, list);
-        mySpinner.setAdapter(dataAdapter);
+
 
 
     }
